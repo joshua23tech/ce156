c2b91f12ac91322bbd5e639e960a7f606082130f
joshua23tech <joshua23.tech@gmail.com>
2025-03-08 20:29:22 +0000

    Coursework Exercise 1

---
+++ exercise_1.py
@@ -0,0 +1,24 @@
+from datetime import date
+import re
+
+def current_age():
+    str_dob = input("What is your date of birth? Please enter your date of birth in the American format: mm/dd/yyyy\n>")
+    regex = re.compile(r'[0-9]{2}/[0-9]{2}/[0-9]{4}')
+    result = regex.fullmatch(str_dob)
+    date_today = date.today()
+    if result:
+        lst_chars_dob = str_dob.split("/")
+        lst_ints_dob = [int(ele) for ele in lst_chars_dob]
+        if lst_ints_dob[0] in list(range(1, 13)):
+            if lst_ints_dob[1] in list(range(1, 32)):
+                if lst_ints_dob[2] in list(range(1, date_today.year + 1)):
+                    formatted_dob = date(int(lst_chars_dob[2]), int(lst_chars_dob[0]), int(lst_chars_dob[1]))
+                    if formatted_dob < date_today:
+                        days_in_year = 365.2425
+                        age = int((date_today - formatted_dob).days / days_in_year)
+                        return f"Today on {str(date_today.day) + "/" + str(date_today.month) + "/" + str(date_today.year)} the User is {age}"
+
+    return f"The date of birth you have entered of {str_dob} is inappropriate"
+
+
+print(current_age())
